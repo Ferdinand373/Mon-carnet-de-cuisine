5ff2b275d99ce4fdf351a7ed840492a88088e2b8
Ferdinand373 <freddycogrel@live.com>
2026-07-27 16:22:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mon-carnet-cuisine-v2.2.0-categories';
+const CACHE_NAME = 'mon-carnet-v2-2-1';
 const APP_SHELL = ['./', './index.html', './mon-carnet-v17.png'];
 
 self.addEventListener('install', event => {
