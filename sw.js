6ddf67db7bea37f129a96c7048cbab0867b4f701
Ferdinand373 <freddycogrel@live.com>
2026-07-27 16:30:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mon-carnet-v2-2-1';
+const CACHE_NAME = 'mon-carnet-v2-3';
 const APP_SHELL = ['./', './index.html', './mon-carnet-v17.png'];
 
 self.addEventListener('install', event => {
